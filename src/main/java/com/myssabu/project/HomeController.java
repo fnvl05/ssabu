99fec7bc8c4ddadd39027911932cbd183049df9c
fnvl05 <fnvl05@naver.com>
2020-06-07 18:30:14 +0900

    최초 커밋

---
+++ src/main/java/com/myssabu/project/HomeController.java
@@ -0,0 +1,15 @@
+package com.myssabu.project;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/home.do")
+	public String home() {
+		return "home";
+	}
+}
